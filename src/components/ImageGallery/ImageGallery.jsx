a326f90caed349da3b9edb2ec766ba754da683e9
Kateryna Verveda <katushka211@gmail.com>
2023-05-08 15:02:13 +0200

    create ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,28 @@
-// export const ImageGallery() => {
+import { Component } from 'react';
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 
-// }
+export class ImageGallery extends Component {
+  state = {
+    name: '',
+    images: [],
+    status: Status.IDLE,
+    error: null,
+    page: 1,
+    totalPages: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.imageName;
+    const nextName = this.props.imageName;
+    if (prevName !== nextName) {
+      this.setState({
+        status: Status.PENDING,
+      });
+    }
+  }
+}
